03abb5dfe8bc19df6fd5bd2b8fac620235b3dc86
Dinkenser12 <133625415+Dinkenser12@users.noreply.github.com>
2024-05-25 11:19:51 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -11,7 +11,7 @@ const {
     delay,
     Browsers,
     makeCacheableSignalKeyStore
-} = require("@whiskeysockets/baileys");
+} = require("@wmaher-zubair-baileys");
 
 function removeFile(FilePath) {
     if (!fs.existsSync(FilePath)) return false;
