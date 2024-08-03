e968e9717b62b10fc7ae687f38d41557f2dcdd53
Eypz God <168025354+eypz@users.noreply.github.com>
2024-08-03 16:32:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "",
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb://mongo:UkftaOaflervvKdRmgcTVzYwbXiORaWe@roundhouse.proxy.rlwy.net:33555",
   SESSION_NAME: process.env.SESSION_NAME || "Bot~"
 };
