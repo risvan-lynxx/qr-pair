a1a009b34b4cecbe99a153334f06a5fed4ed999e
Eypz God <168025354+eypz@users.noreply.github.com>
2024-08-03 16:25:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require("body-parser");
+const open = require('open');
 
 const app = express();
 const __path = process.cwd();
