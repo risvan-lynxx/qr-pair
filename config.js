735ca7b372388d6756b15b55150d986271c5e7e1
lLynx <risvanlynx777@gmail.com>
2025-04-09 06:12:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "mongodb://mongo:UkftaOaflervvKdRmgcTVzYwbXiORaWe@roundhouse.proxy.rlwy.net:33555",
-  SESSION_NAME: process.env.SESSION_NAME || "Bot~"
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb://atlas-sql-67d3bb96c7a9dd792d96e3a9-bwbsb.a.query.mongodb.net/sample_mflix?ssl=true&authSource=admin",
+  SESSION_NAME: process.env.SESSION_NAME || "Zen~"
 };
